f963deb9168b0a981257f25f373a8427ab365b8c
Dhyey1995 <dhyeywdipl@gmail.com>
2020-03-21 12:51:21 +0530

    project section dynamic

--- src/components/ProjectSection.jsx
+++ src/components/ProjectSection.jsx
@@ -1,27 +1,49 @@
 import React, { Component } from 'react';
+import Axios from 'axios';
 
 export default class ProjectSection extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            project: [],
+            api_url : 'http://localhost/dhyey_rathod/portfolioapi/storage/app/',
+        }
+    }
+    componentDidMount() {
+        Axios.get('http://localhost/dhyey_rathod/portfolioapi/api/project')
+            .then(response => {
+                this.setState({
+                    project: response.data.data,
+                });
+            });
+    }
     render() {
         return (
             <section className="case-section padding-bottom padding-top">
                 <div className="container">
                     <div className="row mb-30-none justify-content-center">
-                        <div className="col-sm-10 col-md-6 col-lg-4">
-                            <div className="case-item">
-                                <div className="case-thumb">
-                                    <a href="#0">
-                                        <img src="/assets/images/case/case01.jpg" alt="case" />
-                                    </a>
-                                </div>
-                                <div className="case-content">
-                                    <h4 className="title">
-                                        <a href="#0">Ornare ante maecenas scelerisque </a>
-                                    </h4>
-                                    <p>Phasellus quis vel, lacus rutrfusce velit. Id dolorid ves etiam nec ante. Eget magnis aetiam eget mauris dolor mengo this over</p>
+
+                        {this.state.project.map( (oneProject, index) => (
+                            <div className="col-sm-10 col-md-6 col-lg-4" key={index}>
+                                <div className="case-item">
+                                    <div className="case-thumb">
+                                        <a href="#0">
+                                            <img src={this.state.api_url+oneProject.file_name} alt={oneProject.title} />
+                                        </a>
+                                    </div>
+                                    <div className="case-content">
+                                        <h4 className="title">
+                                            <a href="#0">{oneProject.title} </a>
+                                        </h4>
+                                        {/* <p>Phasellus quis vel, lacus rutrfusce velit. Id dolorid ves etiam nec ante. Eget magnis aetiam eget mauris dolor mengo this over</p> */}
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                        <div className="col-sm-10 col-md-6 col-lg-4">
+                        ))}
+
+
+
+                        {/* <div className="col-sm-10 col-md-6 col-lg-4">
                             <div className="case-item">
                                 <div className="case-thumb">
                                     <a href="#0">
@@ -140,7 +162,7 @@ export default class ProjectSection extends Component {
                                     <p>Phasellus quis vel, lacus rutrfusce velit. Id dolorid ves etiam nec ante. Eget magnis aetiam eget mauris dolor mengo this over</p>
                                 </div>
                             </div>
-                        </div>
+                        </div> */}
                     </div>
                     <div className="load-more-group">
                         <a href="case.html" className="load-more-button active custom-button">Load More Case</a>
